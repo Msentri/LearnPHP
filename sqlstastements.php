cbb63fb15f90925a0f9c66a1a9b8c742dc37bbbb
Sandile <sandile@indigo.co.za>
2016-03-22 15:52:21 +0200

    sql statement

---
+++ sqlstastements.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msentri
+ * Date: 2016/03/22
+ * Time: 3:47 PM
+ */
+
+$insert = "INSERT INTO table (fullnames,email,password) VALUES ('Sandile Mazibuko',
+            'sandile@gmail.com','sandile')";
+
+$update = "UPDATE table fullnames='sandile msentri sandile' WHERE id='3'";
+
+$delete = "DELETE FROM table WHERE id='3'";
+
+$read = "SELECT * FROM table WHERE id='3'";
+
